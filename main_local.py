0960ee8cc8e8fa23e6ad8153217c576ee20e25ef
Renan L <renanlopesdasilva@gmail.com>
2023-03-26 01:39:28 -0300

    upload project

--- main_local.py
+++ main_local.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 # Conexão com o banco de dados "transactional"
 try:
-    transactional_db_string = "postgresql://postgres:password@transactional:5432/dvdrental"
+    transactional_db_string = "postgresql://postgres:password@localhost:5432/dvdrental"
     transactional_engine = create_engine(transactional_db_string)
     transactional_Session = sessionmaker(bind=transactional_engine)
     transactional_session = transactional_Session()
@@ -15,7 +15,7 @@ except:
 
 # Conexão com o banco de dados "analytics"
 try:
-    analytics_db_string = "postgresql://postgres:password@analytics:5432/analytics"
+    analytics_db_string = "postgresql://postgres:password@localhost:5440/analytics"
     analytics_engine = create_engine(analytics_db_string)
     analytics_Session = sessionmaker(bind=analytics_engine)
     analytics_session = analytics_Session()
@@ -24,10 +24,8 @@ except:
     print("PROBLEMA NA CONEXAO AO BD ANALYTICS!!!")
 
 #--
-#As conexões foram realizadas com o nome dos serviços e ambas com a porta 5432 devido ao fato do container
-#que contem o Python estar na mesma rede que os demais.
+#As conexões foram realizadas com as portas que foram mapeadas no compose.
 #--
-
 
 # Query para selecionar todas as tabelas do banco de dados "transactional"
 tables_query = """
